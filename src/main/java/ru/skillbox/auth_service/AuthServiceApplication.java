5b5d1545e848741d0e9b8239c30f79a33fa28cd0
Abriel <abrielz11@gmail.com>
2024-10-31 17:28:18 +0300

    mileStone 3.9:
    docker-compose.yml was cloned to project root and modified server accordingly

--- src/main/java/ru/skillbox/auth_service/AuthServiceApplication.java
+++ src/main/java/ru/skillbox/auth_service/AuthServiceApplication.java
@@ -3,6 +3,7 @@ package ru.skillbox.auth_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AuthServiceApplication {
 
